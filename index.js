666adcd0f9fa85a640eff27e4690ecc144b9fcf4
DylanParenzee <dylanparenzee@hotmail.com>
2022-06-11 15:22:39 +1000

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution here
+
+
+function findMatching(drivers, name){
+    return drivers.filter((drivers) => drivers.toLowerCase() === name.toLowerCase())
+}
+
+
+function fuzzyMatch(drivers, name){
+    return drivers.filter((drivers) => drivers[0] === name[0] && drivers[1] === name[1])
+}
+
+function matchName(drivers, str){
+    return drivers.filter((drivers) => drivers.name === str)
+
+}
